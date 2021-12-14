d1fd5e1b569c033b20f2d046e5e781a2e423547b
Jaime <jmeferns@gmail.com>
2021-12-14 16:37:30 +1100

    Initial Commit

---
+++ LearningGitHun/LearningGitHunApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGitHunApp.swift
+//  LearningGitHun
+//
+//  Created by Jaime Fernandes on 14/12/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningGitHunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
